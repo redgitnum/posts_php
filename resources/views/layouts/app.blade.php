aab030eea2de5fe6ca9c45ca388948b4723f564f
rednum <rednum89@gmail.com>
2021-01-09 17:44:04 +0100

    database basic config, migrations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,34 @@
     <title>Php posts test</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="bg-white flex justify-between text-xl px-6 mb-3">
+        <ul class="flex">
+            <li class="p-6">
+                <a href="/">Home</a>
+            </li>
+            <li class="p-6">
+                <a href="/dashboard">Dashboard</a>
+            </li>
+            <li class="p-6">
+                <a href="/posts">Posts</a>
+            </li>
+        </ul>
+        <ul class="flex">
+            <li class="p-6">
+                <a href="/">David</a>
+            </li>
+            <li class="p-6">
+                <a href="/login">Login</a>
+            </li>
+            <li class="p-6">
+                <a href="/register">Register</a>
+            </li>
+            <li class="p-6">
+                <a href="/logout">Logout</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    index    
+    <div class="flex justify-center">
+
+        <div class="bg-white rounded-lg w-8/12 p-3">
+            <h1 class="text-lg">Posts</h1>
+        </div>
+    </div>  
 @endsection
